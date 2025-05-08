d557564f6963f4b372e50d3378ecb38c80072f9a
hariharan <31499070+Hariharanu@users.noreply.github.com>
2025-05-08 22:30:16 +0530

    Feature/development (#3)
    * Creating controllers
    
    * Adding docker file
    
    * updating workflow file
    
    * docker build and push
    
    * build image on push

--- app.js
+++ app.js
@@ -5,16 +5,17 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import helmet from 'helmet';
 import morgan from 'morgan';
+import authRoute from './Routes/authRoute.js';
 
 dotenv.config()
-morgan('combined')
 
 const app = express()
 app.use(cors())
 app.use(helmet())   
 app.use(bodyParser.json())
+app.use(morgan('combined'))
 
-console.log(process)
+app.use('/auth', authRoute)
 
 mongoose.connect(process.env.MONGO_URI).then(()=>{
     app.listen(process.env.PORT, () => {
